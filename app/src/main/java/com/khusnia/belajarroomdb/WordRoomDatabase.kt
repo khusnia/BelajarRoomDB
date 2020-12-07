b2a43275f4b77b8b378a35d2d1eff0a3c3f43474
khusnia <khusnianur.07@gmail.com>
2020-12-07 15:15:33 +0700

    Initial commit

---
+++ app/src/main/java/com/khusnia/belajarroomdb/WordRoomDatabase.kt
@@ -0,0 +1,59 @@
+package com.khusnia.belajarroomdb
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase(){
+    abstract fun wordDao(): WordDao
+
+
+    private class  WordDataBaseCallBack (
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback(){
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+        suspend fun populateDatabase(wordDao: WordDao){
+            wordDao.deleteALL()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+        }
+    }
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context,
+                        scope: CoroutineScope
+        ): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(context.applicationContext,WordRoomDatabase::class.java,
+                    "word_database")
+                    .addCallback(WordDataBaseCallBack(scope))
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
